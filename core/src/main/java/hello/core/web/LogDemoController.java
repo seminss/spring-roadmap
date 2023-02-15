c94239d3c508123b7f151c5da07d019d51047e56
seminss <2007ksm@naver.com>
2023-02-15 19:42:46 +0900

    기본편 끝~

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,9 +18,9 @@ public class LogDemoController {
     @RequestMapping("log-demo")
     @ResponseBody //화면없이 문자 출력
     public String logDemo(HttpServletRequest request){ //java에서 제공하는 표준 서블릿 정보에 해당하는 request 정보를 받을 수 있다.
-
         String requestURL = request.getRequestURL().toString(); //고객이 어떤 url로 요청했는지 알 수 있다.
-        myLogger.setRequestURL(requestURL); //[http://localhost:8080/log-demo] 출력하게 하기 위함
+        System.out.println("myLogger = " + myLogger);
+        myLogger.setRequestURL(requestURL);
         myLogger.log("controller test");
         logDemoService.logic("testID");
         return "OK";
